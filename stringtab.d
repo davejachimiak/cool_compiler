57f7a249403d577acd195b56350cbdd4d0a60314
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-05-30 18:26:42 -0700

    Remove unneeded files

--- stringtab.d
+++
@@ -1,5 +0,0 @@
-stringtab.o stringtab.d : stringtab.cc /usr/class/cs143/cool/include/PA2/copyright.h \
- stringtab_functions.h /usr/class/cs143/cool/include/PA2/cool-io.h \
- /usr/class/cs143/cool/include/PA2/copyright.h stringtab.h \
- /usr/class/cs143/cool/include/PA2/list.h \
- /usr/class/cs143/cool/include/PA2/cool-io.h
